889f2a11519686e2a620e1fad10ae83ce797f9c6
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-17 14:46:33 -0600

    Update tests using factory bot syntax

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,37 +3,41 @@ require 'rails_helper'
 RSpec.describe 'Application Show Page' do
   describe 'Application details' do
     before :each do
-      @app = Application.create!(name: 'Ezzedine Alwafai', street_address: '1010 Main St', city: 'Memphis', state: 'Tennessee', zip_code: '38134', description: 'I love doggies', status: :in_progress)
+      @app = create(:application)
       @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-      @clyde = @app.pets.create!(adoptable: true, age: 5, breed: 'Bengal Cat', name: 'Clyde', shelter: @shelter)
-      @stomper = @app.pets.create!(adoptable: false, age: 2, breed: 'Ragdoll', name: 'Stomper', shelter: @shelter)
+      @pet_1 = create(:pet, shelter_id: @shelter.id)
+      @pet_2 = create(:pet, shelter_id: @shelter.id)
     end
 
     it 'displays application information' do
+      # app = create(:application)
+      # shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      # clyde = app.pets.create!(adoptable: true, age: 5, breed: 'Bengal Cat', name: 'Clyde', shelter: shelter)
+      # stomper = app.pets.create!(adoptable: false, age: 2, breed: 'Ragdoll', name: 'Stomper', shelter: shelter)
 
       visit "/applications/#{@app.id}"
 
       expect(page).to have_content(@app.name)
-      expect(page).to have_content('1010 Main St, Memphis, Tennessee, 38134')
+      expect(page).to have_content(@app.street_address)
       expect(page).to have_content(@app.description)
-      expect(page).to have_content('Application status: In Progress')
+      expect(page).to have_content(@app.status)
     end
 
     it 'displays the names of all the pets for this application' do
 
       visit "/applications/#{@app.id}"
 
-      expect(page).to have_content(@clyde.name)
-      expect(page).to have_content(@stomper.name)
+      expect(page).to have_content(@pet_1.name)
+      expect(page).to have_content(@pet_2.name)
     end
 
     it 'shows pet name as a link to each of their show page' do
 
      visit "/applications/#{@app.id}"
 
-     click_link 'Clyde'
+     click_link "#{@pet_1.name}"
 
-     expect(current_path).to eq("/pets/#{@clyde.id}")
+     expect(current_path).to eq("/pets/#{@pet_1.id}")
    end
   end
 end
