856b8bf58140dfe86a309561fe4ded0836179eb3
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-17 14:47:36 -0600

    Define factory bot and faker for application and pet

---
+++ spec/factories/pet.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :pet do
+    name { Faker::Creature::Dog.name }
+    breed  { Faker::Creature::Dog.breed }
+    adoptable { true }
+    age { Faker::Number.between(from: 1, to: 10) }
+  end
+end
