245fce9a481ca10301a5c2161c98a0f63966382e
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-16 18:48:11 -0600

    Create applications controller

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationsController < ApplicationController
+
+  def index
+    @applications = Application.all
+  end
+
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    application = Application.create!(application_params)
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zip_code, :description)
+  end
+end
