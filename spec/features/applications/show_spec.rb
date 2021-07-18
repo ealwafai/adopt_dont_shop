acb9117c92719f34d1e89ef7466656a8f9397fdd
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-18 13:29:27 -0600

    Create tests for search and adopt buttons

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,42 +2,78 @@ require 'rails_helper'
 
 RSpec.describe 'Application Show Page' do
   describe 'Application details' do
-    before :each do
-      @app = create(:application)
-      @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-      @pet_1 = create(:pet, shelter_id: @shelter.id)
-      @pet_2 = create(:pet, shelter_id: @shelter.id)
-    end
 
     it 'displays application information' do
-      # app = create(:application)
-      # shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-      # clyde = app.pets.create!(adoptable: true, age: 5, breed: 'Bengal Cat', name: 'Clyde', shelter: shelter)
-      # stomper = app.pets.create!(adoptable: false, age: 2, breed: 'Ragdoll', name: 'Stomper', shelter: shelter)
+      application = create(:application)
 
-      visit "/applications/#{@app.id}"
+      visit "/applications/#{application.id}"
 
-      expect(page).to have_content(@app.name)
-      expect(page).to have_content(@app.street_address)
-      expect(page).to have_content(@app.description)
-      expect(page).to have_content(@app.status)
+      expect(page).to have_content(application.name)
+      expect(page).to have_content(application.street_address)
+      expect(page).to have_content(application.city)
+      expect(page).to have_content(application.state)
+      expect(page).to have_content(application.zip_code)
+      expect(page).to have_content(application.description)
+      expect(page).to have_content("Application status: In Progress")
     end
 
-    it 'displays the names of all the pets for this application' do
+    it 'user sees a section to search for pet by name' do
 
-      visit "/applications/#{@app.id}"
+      Pet.destroy_all
+      Application.destroy_all
+      PetApplication.destroy_all
 
-      expect(page).to have_content(@pet_1.name)
-      expect(page).to have_content(@pet_2.name)
+      application = create(:application)
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet = application.pets.create!(name: 'Clyde', breed: "Bengal", age: 5, adoptable: true, shelter: shelter)
+
+      visit "applications/#{application.id}"
+
+      expect(page).to have_content("Add a Pet to this Application")
+
+      fill_in "search", with: pet.name
+
+      click_on 'Submit'
+
+      expect(page).to have_content("Name: #{pet.name}")
+      expect(page).to have_content("Breed: #{pet.breed}")
+      expect(page).to have_content("Age: #{pet.age}")
+      expect(page).to have_content("Adoptable: #{pet.adoptable}")
     end
 
-    it 'shows pet name as a link to each of their show page' do
+    it 'user sees a button next to each pet to add pet to the application' do
+      application = create(:application)
+      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet = application.pets.create!(name: 'Clyde', breed: "Bengal", age: 5, adoptable: true, shelter: shelter)
 
-     visit "/applications/#{@app.id}"
+      visit "applications/#{application.id}"
 
-     click_link "#{@pet_1.name}"
+      fill_in "search", with: pet.name
+      click_on 'Submit'
 
-     expect(current_path).to eq("/pets/#{@pet_1.id}")
-   end
+      expect(page).to have_button("Adopt: #{pet.name}")
+    end
+
+    it 'user can click on adopt button to add a pet to the application' do
+      Shelter.destroy_all
+      Pet.destroy_all
+      Application.destroy_all
+
+      application = create(:application)
+      shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: true, rank: 9)
+      pet = application.pets.create!(name: 'Clyde', breed: "Bengal", age: 5, adoptable: true, shelter: shelter)
+
+      visit "/applications/#{application.id}"
+
+      fill_in "search", with: pet.name
+      click_on 'Submit'
+      click_on "Adopt: #{pet.name}"
+
+      expect(current_path).to eq("/applications/#{application.id}")
+      expect(page).to have_content("Pets Applied For:")
+      expect(page).to have_link("#{pet.name}")
+      expect(PetApplication.last.application_id).to eq(application.id)
+      expect(PetApplication.last.pet_id).to eq(pet.id)
+    end
   end
 end
