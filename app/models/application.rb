884a426295e6cb72e0874924486d4ca61c6787fc
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-19 16:22:41 -0600

    Add validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -8,5 +8,6 @@ class Application < ApplicationRecord
   validates :state, presence: true
   validates :zip_code, presence: true
   validates :description, presence: true
+  validates :status, presence: true
 
 end
