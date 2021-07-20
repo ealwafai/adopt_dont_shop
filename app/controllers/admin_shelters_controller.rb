bca1d79ed6b97b5004dad05b8e91b61b319a3080
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-20 15:30:36 -0600

    Add pending

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,5 +2,6 @@ class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.all
     @reversed_shelters = @shelters.shelters_in_reverse
+    @pending = @shelters.shelters_pending_apps
   end
 end
